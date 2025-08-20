64eec78ae3fbeac64d31db970534ddb4d77d9fcd
Kvsvicentini <kaua.vicentini@gmail.com>
2025-08-19 23:46:47 -0300

    Primeiro commit

---
+++ src/main/java/com/example/demo/ProjetoDsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDsApplication.class, args);
+	}
+
+}
