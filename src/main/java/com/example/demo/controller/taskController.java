4e64ebeed5e4d9827f0e6560d16116c0e1d497e9
Kvsvcientini <kaua.vicentini@gmail.com>
2025-08-27 22:21:42 -0300

    Tentando rodar isso

--- src/main/java/com/example/demo/controller/taskController.java
+++ src/main/java/com/example/demo/controller/taskController.java
@@ -17,18 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class taskController {
 
     private List<Task> repository = new ArrayList<>();
-    
-    @GetMapping("/tasks")
-    public String listTasks(Model model) {
 
-        var tasks = List.of(
-            new Task(1L, "Criar BD", "Criar bando mysql", 50, 0),
-            new Task(1L, "Prototipo", "Montar o figma", 40, 0),
-            new Task(1L, "Deploy", "Colocar em produção",1050, 0)
-            );
-
-        model.addAttribute("tasks", tasks);
-
+    @GetMapping
+    public String listTasks(Model model){
+        model.addAttribute("tasks", repository);
         return "tasks";
     }
 
